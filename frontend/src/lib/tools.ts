36a66fad50d6b148da78ee83e6aa67d2b4549932
Alexjes98 <alexjes6543@gmail.com>
2026-03-08 22:46:04 -0400

    feat: Add `get_app_info` tool to provide a user guide and application information.

--- frontend/src/lib/tools.ts
+++ frontend/src/lib/tools.ts
@@ -1,6 +1,7 @@
 import { tool } from "@langchain/core/tools";
 import { z } from "zod";
 import { IndexedDbRepository } from "@/api/IndexedDbRepository";
+import { profile } from "console";
 
 export const createSearchMovementsTool = () => {
   return tool(
@@ -79,6 +80,27 @@ export const createGetCategoriesTool = () => {
       description:
         "Get a list of all available categories. Use this to find category IDs context to filter transactions when using the search_movements tool.",
       schema: z.object({}),
+    },
+  );
+};
+
+export const createGetAppInfoTool = () => {
+  return tool(
+    () => {
+      return `
+      AppInfo: The finance app is builded so the user should first populate the common persons he interacts with everyday, some common expenses and income categories and create some accounts, The accounts should represent real accounts the user has, e.g. bank account, credit card, cash, etc. The categories should represent real categories the user has, e.g. food, rent, salary, etc. The persons should represent real persons the user has, e.g. family, friends, colleagues, etc.
+      Dashboard: Get a quick overview of your finances here. Contains an account general balance, and actual month balance, expenses and incomes.
+      Transactions: View and manage all your income and expenses. You can filter by search term, category, account, dates, and type.
+      Manage: Organize your accounts, categories, and people here. Containes information about accounts, categories and people. Should be populated before the transactions because they are linked to them.
+      Insights: Analyze your financial trends and summaries over time. Contains a monthly resume with common transactions by category, and a list of transactions sorted by category and a Yearly resume with the general balance for each month of the year
+      Assistant: Chat with your AI financial assistant for personalized help.
+      Profile: Manage your profile and settings. Contains information about your profile and settings.`;
+    },
+    {
+      name: "get_app_info",
+      description:
+        "Provide a user guide for the application. Use this when the user ask questions related on how to do something or has question of some page in particular e.g ",
+      schema: z.string(),
     },
   );
 };
